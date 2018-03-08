a427fb8a5af81681a07f0406bcb625b047470ac2
ToniTJK <toninotor5@gmail.com>
2018-03-08 19:22:20 +0100

    Menu estructurado

--- src/main/java/pizzeria/pizzeria/Menu.java
+++ src/main/java/pizzeria/pizzeria/Menu.java
@@ -14,11 +14,11 @@ public class Menu {
         
         while (!salir) {
  
-            System.out.println("1. Dar de alta a los clientes.");
-            System.out.println("2. Dar de alta facturas de los clientes.");
-            System.out.println("3. Consultar un cliente y obtener todas sus facturas.");
-            System.out.println("4. Consultar todas las facturas que aún no se han pagado.");
-            System.out.println("5. Salir del programa.");
+            System.out.println("1. Crear Usuario");
+            System.out.println("2. Login");
+            System.out.println("3. Pedir Pizza");
+            System.out.println("4. Modificar Pedido");
+            System.out.println("5. Eliminar Pedido");
  
             try {
  
@@ -27,30 +27,44 @@ public class Menu {
  
                 switch (opcion) {
                     case 1:
-                        System.out.println("Por favor, introduce el nombre del cliente al que quieres dar de alta:");
+                        System.out.println("Por favor, introduce tu nombre:");
+                        String name = scanner.next();
+                        System.out.println("Por favor, introduce tu contraseña:");
+                        String password = scanner.next();
+                        
+                        //Insert Usuario (Comprobar que no exista ya)
                         break;
                     case 2:
-                        System.out.println("Por favor, introduce la fecha de la factura:");
-                        break;
-                    case 3:
-                        System.out.println("Escribe el nombre del cliente:");
+                    	
+                    	while(true) { 
+                    	System.out.println("Por favor, introduce tu nombre:");
+                        String name1 = scanner.next();
+                        System.out.println("Por favor, introduce tu contraseña:");
+                        String password1 = scanner.next();
+                        	
+                        //TODO CONSULTA -> Comprobar si existe el usuario BREAK if not bucle again.
+                        //break;
+                    	}
                         
-                        
-                        
-                        
-                        
+				case 3:
+                        System.out.println("A continuación le mostramos nuestras diferentes pizzas: (Escriba el nombre de la que quiere)");
+                        //TODO CONSULTA -> Diferentes pizzas AND Insert in Pedido.
                         break;
                     case 4:
-                        System.out.println("Has seleccionado la opcion 4");
-                        
+                        System.out.println("Elige el pedido que quieres modificar: ");
+                      //TODO CONSULTA -> Diferentes pedidos AND Update in Pedido correspondiente.
                         
                         
                         break;
                     case 5:
+                    	System.out.println("Elige el pedido que quieres eliminar: ");
+                        //TODO CONSULTA -> Diferentes pedidos AND Delete in Pedido correspondiente.
+                        break;
+                    case 6:
                         salir = true;
                         break;
                     default:
-                        System.out.println("Sólo números entre 1 y 5, por favor.");
+                        System.out.println("Sólo números entre 1 y 6, por favor.");
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Tienes que insertar un número.");
--- src/main/java/pizzeria/pizzeria/test.java
+++
@@ -1,4 +0,0 @@
-package pizzeria.pizzeria;
-
-public class test {
-}
